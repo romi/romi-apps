5b13b7d6f1ebb50eeeebc2c8068aaa492249d19b
hanappe <peter@hanappe.com>
2025-08-26 18:23:14 +0200

    moving to systemd

--- systemd/rcom-registry-start.sh
+++ systemd/rcom-registry-start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-/home/romi/romi-apps/build/bin/romi-registry > /home/romi/romi-registry.log 2>&1 &
-echo $! > /home/romi/romi-registry.pid
+/home/romi/romi-apps/build/bin/rcom-registry > /home/romi/rcom-registry.log 2>&1 &
+echo $! > /home/romi/rcom-registry.pid
 
 exit 0
--- systemd/rcom-registry-stop.sh
+++ systemd/rcom-registry-stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if [ -e "/home/romi/romi-registry.pid" ]; then
-    pid=$(cat /home/romi/romi-registry.pid)
+if [ -e "/home/romi/rcom-registry.pid" ]; then
+    pid=$(cat /home/romi/rcom-registry.pid)
     if [ -e "/proc/$pid/cmdline" ]; then
         kill -9 $pid
     fi
