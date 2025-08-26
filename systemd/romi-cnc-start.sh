9c418cad04ef4b90e1e6b6dad2987abc088b81d6
hanappe <peter@hanappe.com>
2025-08-26 18:26:16 +0200

    moving to systemd

--- systemd/romi-cnc-start.sh
+++ systemd/romi-cnc-start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-/home/romi/romi-apps/build/bin/romi-cnc --session-directory /home/romi > /home/romi/romi-cnc.log 2>&1 &
+/home/romi/romi-apps/build/bin/romi-cnc --directory /home/romi > /home/romi/romi-cnc.log 2>&1 &
 echo $! > /home/romi/romi-cnc.pid
 
 exit 0
