3c9ee47708cd9fd44ab604c0834dd62cb573702c
hanappe <peter@hanappe.com>
2025-08-28 16:00:28 +0200

    add battery

--- python/src/romi/battery.py
+++ python/src/romi/battery.py
@@ -3,24 +3,24 @@ from rcom.rcom_client import RcomWSClient
 import argparse
 
 
-class BatteryMonitor():
+class Battery():
 
     @staticmethod
     def create(topic, registry_ip = None):
         client = RcomWSClient(topic, topic, registry_ip)
-        return BatteryMonitor(client)
+        return Battery(client)
    
     def __init__(self, client):
         self.client = client
         
     def is_charging(self):
-        return self.client.execute('battery-monitor:is-charging')
+        return self.client.execute('battery:is-charging')
         
     def get_voltage(self):
-        return self.client.execute('battery-monitor:get-voltage')
+        return self.client.execute('battery:get-voltage')
         
     def get_current(self):
-        return self.client.execute('battery-monitor:get-current')
+        return self.client.execute('battery:get-current')
 
         
 if __name__ == '__main__':
@@ -31,9 +31,10 @@ if __name__ == '__main__':
                         help='The IP address of the registry')
     args = parser.parse_args()
     
-    monitor = BatteryMonitor.create(args.topic, args.registry)
+    battery = Battery.create(args.topic, args.registry)
     while True:
-        print(f"Charging: {monitor.is_charging()}")
-        print(f"Voltage:  {monitor.get_voltage()} V")
-        print(f"Current:  {monitor.get_current() A}")
+        print(f"Charging: {battery.is_charging()}")
+        print(f"Voltage:  {battery.get_voltage()} V")
+        print(f"Current:  {battery.get_current()} A")
+        print(f"Level:    {100.0 * battery.get_level()} %")
         time.sleep(1)
