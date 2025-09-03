29a9530275d678a170afe0631fcd5999cafb6eba
hanappe <peter@hanappe.com>
2025-09-03 20:51:31 +0200

    update every 5s

--- apps/romi-battery/LithiumBattery.cpp
+++ apps/romi-battery/LithiumBattery.cpp
@@ -93,7 +93,7 @@ namespace romi {
                 timestamp_ = ClockAccessor::GetInstance()->time();
                 while (!done_) {
                         updated_locked();
-                        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+                        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
                 }
         }
 
